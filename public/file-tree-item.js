2274cb49358725b93635554fecec41be5f62d529
Darlan Alves <me@darlanalv.es>
2026-09-23 12:22:48 +0000

    Hoist tree item host reference

--- public/file-tree-item.js
+++ public/file-tree-item.js
@@ -1,8 +1,8 @@
 import { defineProp, getElement } from '@li3/web';
 export default function () {
   const item = defineProp('item', { default: () => ({}) });
+  const host = getElement();
   function select() {
-    const host = getElement();
     const value = item.value?.path ? item.value : { path: host.dataset.path, type: host.dataset.type, name: host.dataset.name, depth: Number(host.dataset.depth || 0), open: host.dataset.open === 'true', children: [] };
     if (!value?.path) return;
     getElement().dispatchEvent(new CustomEvent('tree-select', { bubbles: true, composed: true, detail: value }));
